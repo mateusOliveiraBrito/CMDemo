dca2ff16e9966c0b4aa4e89a03ea12831a7e9c57
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2021-04-08 16:08:42 -0300

    Implementa PersonModel Realiza implementação de comboBox com base em uma lista de pessoas e recupera um objeto selecionado dessa lista

---
+++ WPFUI/Models/PersonModel.cs
@@ -0,0 +1,8 @@
+﻿namespace WPFUI.Models
+{
+    public class PersonModel
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+    }
+}
--- WPFUI/ViewModels/ShellViewModel.cs
+++ WPFUI/ViewModels/ShellViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Caliburn.Micro;
+using WPFUI.Models;
 
 namespace WPFUI.ViewModels
 {
@@ -6,6 +7,22 @@ namespace WPFUI.ViewModels
     {
         private string _firstName = "Mateus";
         private string _lastName = "Oliveira Brito";
+        private BindableCollection<PersonModel> _people;
+        private PersonModel _peopleSelected;
+
+        public ShellViewModel()
+        {
+            _people = new BindableCollection<PersonModel>();
+
+            People.Add(new PersonModel() { Name = "Mateus", LastName = "Oliveira Brito" });
+            People.Add(new PersonModel() { Name = "Altair", LastName = "Garcia" });
+            People.Add(new PersonModel() { Name = "André", LastName = "Baltieri" });
+            People.Add(new PersonModel() { Name = "Marcos", LastName = "Paulo" });
+            People.Add(new PersonModel() { Name = "Ana", LastName = "Clara" });
+            People.Add(new PersonModel() { Name = "Jose", LastName = "Silva" });
+            People.Add(new PersonModel() { Name = "Lúcio", LastName = "Malfoy" });
+            People.Add(new PersonModel() { Name = "Alastor", LastName = "Moody" });
+        }
 
         public string FullName { get => $"{FirstName} {LastName}"; }
 
@@ -37,5 +54,22 @@ namespace WPFUI.ViewModels
                 NotifyOfPropertyChange(() => FullName);
             }
         }
+
+        public BindableCollection<PersonModel> People
+        {
+            get { return _people; }
+            set { _people = value; }
+        }
+
+        public PersonModel PeopleSelected
+        {
+            get { return _peopleSelected; }
+            set
+            {
+                _peopleSelected = value;
+                NotifyOfPropertyChange(() => PeopleSelected);
+            }
+        }
+
     }
 }
