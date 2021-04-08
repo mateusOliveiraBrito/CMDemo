05f290ef48761502ad5ce670cc55f87e63a75282
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2021-04-08 15:02:23 -0300

    Adiciona propriedades de sobrenome e nome completo afim de realizar o binding das propriedades e verificar a aplicação do método NotifyPropertyChanged() para retornar um nome completo dinâmico na aplicação

--- WPFUI/ViewModels/ShellViewModel.cs
+++ WPFUI/ViewModels/ShellViewModel.cs
@@ -5,6 +5,9 @@ namespace WPFUI.ViewModels
     public class ShellViewModel : Screen
     {
         private string _firstName = "Mateus";
+        private string _lastName = "Oliveira Brito";
+
+        public string FullName { get => $"{FirstName} {LastName}"; }
 
         public string FirstName
         {
@@ -16,6 +19,22 @@ namespace WPFUI.ViewModels
             set
             {
                 _firstName = value;
+                NotifyOfPropertyChange(() => FullName);
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+
+            }
+
+            set
+            {
+                _lastName = value;
+                NotifyOfPropertyChange(() => FullName);
             }
         }
     }
